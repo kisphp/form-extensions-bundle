653299bcb8188dd4947dafecc8b9c3d4d0fe94da
Bogdan Rizac <mariusbogdan83@gmail.com>
2017-07-23 10:20:21 +0200

    add bundle

---
+++ src/FormExtensionBundle/FormExtensionBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Kisphp\FormExtensionBundle;
+
+use ComunicaBundle\DependencyInjection\Compiler\TwigFormPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FormExtensionBundle extends Bundle
+{
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new TwigFormPass());
+    }
+}
